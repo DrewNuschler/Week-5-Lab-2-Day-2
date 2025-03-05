e473ba6e8f54eacb935b661ab052a6c4c9873aed
DrewNuschler <lookup1929@gmail.com>
2025-03-04 23:56:45 -0600

    Page-2-done

--- Day-2-Page-2.js
+++ Day-2-Page-2.js
@@ -3,8 +3,8 @@ function catChecker(){
     let name;
     let sick;
     name = prompt("What is your cats name?");
-    sick = prompt("your cat sick");
-    catHeader.innerHTML = `Oh ${name}, Is very sweet.`;
-    catParagraph.innerHTML = `I think ${sick}.`;
+    sick = prompt("Is your cat ok?");
+    catHeader.innerHTML = `Oh ${name}  Is very sweet.`;
+    catParagraph.innerHTML = `I think ${sick} is getting sick.`;
 }
-catButton.oneclick = catChecker;
+catButton.onclick = catChecker;
